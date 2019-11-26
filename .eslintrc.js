89ecdfdc1c8956f46a6b3f34da71507667d8e83a
danyelhenrique <danyelhenriquefidel@gmail.com>
2019-11-25 21:22:33 -0300

    change rules eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     plugins: ['react', 'prettier'],
     rules: {
+        'no-underscore-dangle': 'off',
         'prettier/prettier': 'error',
         'react/jsx-filename-extension': [
             'warn',
